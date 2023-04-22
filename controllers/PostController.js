b35712704eda089ab8779d36a39a0bd27f241d77
SkyKeyUA <veremiienkoov96@gmail.com>
2023-04-22 13:39:56 +0300

    functions getOne(article) / getAll(articles) / update / remove were created

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,67 @@
 /** @format */
 import PostModel from '../models/Post.js';
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec();
+
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to get articles',
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const doc = await PostModel.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { viewsCount: 1 } },
+      { new: true },
+    );
+
+    if (!doc) {
+      return res.status(404).json({
+        message: 'The article was not found',
+      });
+    }
+
+    res.json(doc);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to get an article',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const doc = await PostModel.findOneAndDelete({ _id: postId });
+
+    if (!doc) {
+      return res.status(404).json({
+        message: 'The article was not found',
+      });
+    }
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to remove an article',
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
@@ -18,6 +79,36 @@ export const create = async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: 'Failed to create an article',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const doc = await PostModel.updateOne(
+      { _id: postId },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        user: req.userId,
+      },
+    );
+    if (!doc) {
+      return res.status(404).json({
+        message: 'The article was not found',
+      });
+    }
+    res.json({
+      seccuss: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Failed to update an article',
     });
   }
 };
