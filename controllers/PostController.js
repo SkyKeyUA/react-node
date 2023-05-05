cba9ddb8f7ba872c00583a0b4ceb6813ac230708
SkyKeyUA <veremiienkoov96@gmail.com>
2023-05-05 18:03:02 +0300

    User tracking has been added

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -42,7 +42,7 @@ export const getOne = async (req, res) => {
       { _id: postId },
       { $inc: { viewsCount: 1 } },
       { new: true },
-    );
+    ).populate('user');
 
     if (!doc) {
       return res.status(404).json({
--- validations/post.js
+++ validations/post.js
@@ -5,6 +5,6 @@ import { body } from 'express-validator';
 export const postCreateValidation = [
   body('title', 'Enter the title of the article').isLength({ min: 3 }).isString(),
   body('text', 'Enter the text of the article').isLength({ min: 3 }).isString(),
-  body('tags', 'Incorrect tags format').optional().isArray(),
+  body('tags', 'Incorrect tags format').optional().isString(),
   body('imageUrl', 'Incorrect image link').optional().isString(),
 ];
